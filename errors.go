2beba2fa6cbf67a9bcf4f7f1b052429d0c23f804
xxchan <xxchan@sjtu.edu.cn>
2021-05-12 11:32:12 +0800

    delete error

--- errors.go
+++
@@ -1,8 +0,0 @@
-package dropbox
-
-import "github.com/aos-dev/go-storage/v3/services"
-
-var (
-	// ErrEntryUnexpected is the error returned when Dropbox service has returned an unexpected kind of entry.
-	ErrEntryUnexpected = services.NewErrorCode("unexpected entry")
-)
